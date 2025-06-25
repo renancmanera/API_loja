67fd874b49c1756eab97d4b8daa80ba58a3d143f
Renan Carvalho Manera <rcarvalhomanera@gmail.com>
2025-06-25 17:28:34 -0300

    acrescentando bcryptjs

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -1,97 +1,89 @@
 'use strict';
 
-const ValidationContract = require('../validators/fluent-validator'); // importar o validador
-const repository = require('../repositories/customer-repository'); // importar o repositório
-const md5 = require('md5'); // importar o md5 para criptografar a senha
-const authService = require('../services/auth-service'); // importar o serviço de autenticação
+const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/customer-repository');
+const bcrypt = require('bcryptjs');
+const authService = require('../services/auth-service');
 
-exports.get = async(req, res, next) => {
+exports.get = async (req, res, next) => {
     try {
-        var data = await repository.get();
+        const data = await repository.get();
         res.status(200).send(data);
     } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
     }
-}
+};
 
-exports.post = async(req, res, next) => {
+exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.name, 3, 'O nome deve conter pelo menos 3 caracteres');
     contract.isEmail(req.body.email, 'E-mail inválido');
     contract.hasMinLen(req.body.password, 6, 'A senha deve conter pelo menos 6 caracteres');
 
-    // Se os dados forem inválidos
     if (!contract.isValid()) {
         res.status(400).send(contract.errors()).end();
         return;
     }
 
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY),
+            password: hashedPassword,
             roles: ["user"]
         });
 
-        res.status(201).send({
-            message: 'Cliente cadastrado com sucesso!'
-        });
+        res.status(201).send({ message: 'Cliente cadastrado com sucesso!' });
     } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
     }
 };
 
 exports.put = async (req, res, next) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         await repository.update(req.params.id, {
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: hashedPassword
         });
-        res.status(200).send({
-            message: 'Cliente atualizado com sucesso!'
-        });
+
+        res.status(200).send({ message: 'Cliente atualizado com sucesso!' });
     } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
     }
-}
+};
 
 exports.delete = async (req, res, next) => {
     try {
         await repository.delete(req.params.id);
-        res.status(200).send({
-            message: 'Cliente removido com sucesso!'
-        });
+        res.status(200).send({ message: 'Cliente removido com sucesso!' });
     } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
     }
 };
 
-exports.authenticate = async(req, res, next) => {
+exports.authenticate = async (req, res, next) => {
     try {
-        const customer = await repository.authenticate({
-            email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
-        });
+        const customer = await repository.getByEmail(req.body.email);
+        if (!customer) {
+            res.status(404).send({ message: 'Usuário ou senha inválidos' });
+            return;
+        }
 
-        if(!customer) {
-            res.status(404).send({message: 'Usuário ou senha inválidos'});
+        const isMatch = await bcrypt.compare(req.body.password, customer.password);
+        if (!isMatch) {
+            res.status(404).send({ message: 'Usuário ou senha inválidos' });
             return;
         }
 
         const token = await authService.generateToken({
             id: customer._id,
             email: customer.email,
-            name: customer.name,
+            name: customer.name
         });
 
         res.status(201).send({
@@ -102,9 +94,6 @@ exports.authenticate = async(req, res, next) => {
             }
         });
     } catch (e) {
-        res.status(500).send({
-        message: 'Falha ao processar sua requisição',
-});
-
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
     }
 };
